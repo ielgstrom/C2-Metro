d25c7a915875d282aad7ee6bae89734f27cd7a3c
Ignasi <i.elgstrom@gmail.com>
2021-06-02 10:51:46 +0200

    commit inicial

---
+++ js/index.js
@@ -0,0 +1,9 @@
+fetch(
+  "https://api.tmb.cat/v1/ibus/stops/2775?app_id=9a37b22c&app_key=71045dc36cfb4db6f76e20bb71dc70d6"
+)
+  .then((response) => response.json())
+  .then((datos) => queParadaEs(datos));
+
+const queParadaEs = (paradas) => {
+  console.log(`El bus te tarda ${paradas.data.ibus[0]["text-ca"]}`);
+};
